7a643cf621e06628bb9cd441e09363a4655e174e
David Osorio <davidoso7901@gmail.com>
2024-12-03 22:49:42 -0500

    enviar

--- Reflex_py/Reflex_py.py
+++ Reflex_py/Reflex_py.py
@@ -6,54 +6,18 @@ from Reflex_py.views.botones.botones import *
 
 class State(rx.State):
     valor = [""]
-    union_str: str
+    union_str = ""
     i = 0
     
-    def valores1(self):
-        self.valor.append(1)
+    #NUMEROS [0,1,2,3,4,5,6,7,8,9]
+    def valores(self, x):
+        self.valor.append(x)
     
-    def valores2(self):
-        self.valor.append(2)
+    #SIMBOLOS SIN LOGICA [+,-,*,/,.]
+    def simbol(self, x):
+        self.valor.append(x)
         
-    def valores3(self):
-        self.valor.append(3)
-        
-    def valores4(self):
-        self.valor.append(4)
-    
-    def valores5(self):
-        self.valor.append(5)
-        
-    def valores6(self):
-        self.valor.append(6)
-        
-    def valores7(self):
-        self.valor.append(7)
-        
-    def valores8(self):
-        self.valor.append(8)
-        
-    def valores9(self):
-        self.valor.append(9)
-        
-    def valores0(self):
-        self.valor.append(0)
-    
-    def valoresdot(self):
-        self.valor.append(".")
-    
-    def valoresplus(self):
-        self.valor.append("+")
-        
-    def valoresminus(self):
-        self.valor.append("-")
-    
-    def valoresdivide(self):
-        self.valor.append("/")
-    
-    def valoresmult(self):
-        self.valor.append("*")
-        
+    #LOGICA DEL PORCENTAJE
     def valoresporc(self):
         self.valor.append("%")
         
@@ -65,12 +29,16 @@ class State(rx.State):
                 pass
             else:
                 self.union_str += str(self.i)
-
         self.valor = f"= {eval(self.union_str)/100}"
     
+    #LOGICA DEL BORRAR UNO EN UNO
     def valoresdel(self):
-        self.valor.remove(self.valor[-1])
-        
+        if isinstance(self.valor, list):
+            self.valor.remove(self.valor[-1])
+        else:
+            self.valor = [""]       
+            
+    #LOGICA DEL IGUAL
     def valoresiqual(self):
         self.valor = [str(i) for i in self.valor]
         self.union_str = ""
@@ -78,8 +46,9 @@ class State(rx.State):
         for self.i in self.valor:
             self.union_str += str(self.i)
 
-        self.valor = f"= {eval(self.union_str)}"
-        
+        self.valor = str(f"= {eval(self.union_str)}")
+           
+    #LOGICA DEL BORRAR EN TOTAL
     def clear(self):
         self.valor = [""]
         self.valor.clear()
@@ -118,33 +87,33 @@ def index():
                     botones_icon("refresh-cw-off", on_click=State.clear),
                     botones_icon("circle-x", on_click=State.valoresdel),
                     botones_icon("percent", on_click=State.valoresporc),
-                    botones_icon("divide", on_click=State.valoresdivide),
+                    botones_icon("divide", on_click=State.simbol("/")),
                     margin = "10px",
                 ),
                 rx.hstack(
-                    botones_inferiores("7", on_click=State.valores7),
-                    botones_inferiores("8", on_click=State.valores8),
-                    botones_inferiores("9", on_click=State.valores9),
-                    botones_icon("x", on_click=State.valoresmult),
+                    botones_inferiores("7", on_click=State.valores(7)),
+                    botones_inferiores("8", on_click=State.valores(8)),
+                    botones_inferiores("9", on_click=State.valores(9)),
+                    botones_icon("x", on_click=State.simbol("*")),
                     margin = "10px"
                 ),
                 rx.hstack(
-                    botones_secundarios("4", on_click=State.valores4),
-                    botones_secundarios("5", on_click=State.valores5),
-                    botones_secundarios("6", on_click=State.valores6),
-                    botones_icon("minus", on_click=State.valoresminus),
+                    botones_secundarios("4", on_click=State.valores(4)),
+                    botones_secundarios("5", on_click=State.valores(5)),
+                    botones_secundarios("6", on_click=State.valores(6)),
+                    botones_icon("minus", on_click=State.simbol("-")),
                     margin = "10px"
                 ),
                 rx.hstack(
-                    botones_principales("1", on_click=State.valores1),
-                    botones_principales("2", on_click=State.valores2),
-                    botones_principales("3", on_click=State.valores3),
-                    botones_icon("plus", on_click=State.valoresplus),
+                    botones_principales("1", on_click=State.valores(1)),
+                    botones_principales("2", on_click=State.valores(2)),
+                    botones_principales("3", on_click=State.valores(3)),
+                    botones_icon("plus", on_click=State.simbol("+")),
                     margin = "10px"
                 ),
                 rx.hstack(
-                    botones_principales("0", on_click=State.valores0),
-                    botones_icon("dot", on_click=State.valoresdot),
+                    botones_principales("0", on_click=State.valores(0)),
+                    botones_icon("dot", on_click=State.simbol(".")),
                     botones_icon("equal", on_click=State.valoresiqual),
                     margin = "10px",
                     margin_left = "98px",
